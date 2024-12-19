72b8295fb086141323f6764c0c0685de6b4a1652
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-19 16:34:38 +0100

    Add handleNavigateToEpisodeDetails functionality to EpisdeCard component

--- src/presentation/features/episodes/components/EpisodeCard.tsx
+++ src/presentation/features/episodes/components/EpisodeCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { Episode } from '@entities/Episode'
 
 type PropsType = {
@@ -6,10 +7,16 @@ type PropsType = {
 }
 
 export function EpisodeCard({ item }: PropsType) {
-  const { name, episode, air_date } = item
+  const router = useRouter()
+  const { id, name, episode, air_date } = item
+
+  const handleNavigateToEpisodeDetails = () => {
+    router.push(`episodes/${id}`)
+  }
 
   return (
     <li
+      onClick={handleNavigateToEpisodeDetails}
       data-testid='episode-card'
       aria-labelledby={`episode-title-${item.id}`}
       className='relative p-6 min-h-[20rem] bg-gradient-to-b from-greenPortal via-green-300 to-greenLightPortal hover:from-greenLightPortal hover:via-green-200 hover:to-greenPortal rounded-xl flex flex-col items-center hover:shadow-lg hover:shadow-greenPortal primary-hover-card-transition'
